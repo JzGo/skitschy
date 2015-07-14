d3095f0ec654b73b7592bca239093a8378a09082
Jezreel <jezreel.go@gmail.com>
2015-07-14 11:42:13 -0700

    add some profile factories

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,9 +1,30 @@
 FactoryGirl.define do
   factory :profile do
-    username "MyString"
-tagline "MyString"
-aboutme "MyString"
-genres "MyString"
+    username "Name"
+    tagline "Cool"
+    aboutme "I am"
+    genres "genre"
   end
+
+   factory :scifi do
+     username "Alien"
+     tagline "Another"
+     aboutme "Planet"
+     genres "Science Fiction"
+   end
+
+   factory :fantasy do
+     username "Faun"
+     tagline "Another"
+     aboutme "Land"
+     genres "Fantasy"
+   end
+
+   factory :humor do
+     username "Bananapeel"
+     tagline "whoops"
+     aboutme "snicker"
+     genres "humor"
+   end
 
 end
