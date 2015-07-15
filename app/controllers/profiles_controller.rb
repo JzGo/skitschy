e4af9c5cf6ea1be1c3b2286ff1bc668dcdadd2c8
Jezreel <jezreel.go@gmail.com>
2015-07-15 16:30:40 -0700

    add edit profile functionality

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,18 @@
 class ProfilesController < ApplicationController
+  def edit
+    @profile = Profile.find(current_user.id)
+  end
+
+  def update
+    @profile = Profile.find(current_user.id)
+    if @profile.update(profile_params)
+      redirect_to user_path
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:username, :tagline, :aboutme, :genres)
+  end
 end
