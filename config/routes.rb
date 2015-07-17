15ede3ae54a8fc4696d43d9c8a686f50b4b989af
Jezreel <jezreel.go@gmail.com>
2015-07-17 11:08:00 -0700

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :stories do
-    resources :profiles do
-    end
+    resources :stories
+    resources :profiles
   end
 
   resources :stories, only: [:browse, :show]
-  
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
