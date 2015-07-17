0aa5b04c0e6c6bdff6418e4e00d0fca19882e5dd
Jezreel <jezreel.go@gmail.com>
2015-07-17 15:31:15 -0700

    add list group to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @profile = Profile.find(@user.id)
+    @stories = Story.where(user_id: @user.id)
   end
 end
