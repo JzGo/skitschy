291237f2b46e6fcff99393c881e692715bb74dbb
Jezreel <jezreel.go@gmail.com>
2015-07-17 10:33:28 -0700

    initial story relationships

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :profiles do
     end
-  end
+  end  
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
