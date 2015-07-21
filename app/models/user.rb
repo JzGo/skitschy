43fa925f59ba4ea53a52f37e538d1031876bd024
Jezreel <jezreel.go@gmail.com>
2015-07-20 17:19:38 -0700

    working out unfollow functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_many :stories, dependent: :destroy
   has_many :following_through, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy
   has_many :follower_through, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
-  has_many :followings, through: :follower_through, source: :following
-  has_many :followers, through: :following_through, source: :follower
+  has_many :followings, through: :follower_through, source: :follower
+  has_many :followers, through: :following_through, source: :following
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
