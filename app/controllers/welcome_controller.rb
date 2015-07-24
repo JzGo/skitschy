20d176a15503d0a8d97cf248b37e8272f7cc6acb
Jezreel <jezreel.go@gmail.com>
2015-07-24 10:05:27 -0700

    set stories to current user stories in welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
     @profile = Profile.find_or_create_by!(user_id: current_user.id)
+    @stories = Story.where(user_id: current_user.id)
   end
 end
